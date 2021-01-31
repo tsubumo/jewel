04d85ea2df7cabe8bbce0c1e09252ad858b1c0a6
akira sasaki <sunshine.akira.0215@gmail.com>
2021-01-31 13:58:16 +0900

    ダイアログ表示検証

--- lib/scene/scene_manager.dart
+++ lib/scene/scene_manager.dart
@@ -2,24 +2,27 @@
 // シーン遷移管理
 // --------------------------------------------
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'title.dart';
 import 'home.dart';
+import 'test_dialog.dart';
 
-final String key_title = "title";
-final String key_home = "home";
+const KEY_TITLE = "title";
+const KEY_HOME = "home";
+
 
 // 開始ルート取得
 String getInitialRoute()
 {
-  return key_title;
+  return KEY_TITLE;
 }
 
 // シーン遷移MAP取得
 Map<String, Widget Function(BuildContext)> getRoutes()
 {
   var map = {
-    key_title: (context) => TitleScene(),
-    key_home: (context) => HomeScene(),
+    KEY_TITLE: (context) => TitleScene(),
+    KEY_HOME: (context) => HomeScene(),
   };
   return map;
 }
@@ -28,4 +31,80 @@ Map<String, Widget Function(BuildContext)> getRoutes()
 String getDbgTitle(String title)
 {
   return "[jewel] " + title;
+}
+
+void showTestDIalog(BuildContext context)
+{
+  CustomDialog(
+    context,
+  ).showCustomDialog();
+}
+
+
+// --------------------------------------------
+// ダイアログ管理
+// --------------------------------------------
+class ModalOverlay extends ModalRoute<void> {
+ 
+  // ダイアログ内のWidget
+  final Widget contents;
+ 
+  // Androidのバックボタンを有効にするか
+  final bool isAndroidBackEnable;
+ 
+  ModalOverlay(this.contents, {this.isAndroidBackEnable = true}) : super();
+ 
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 100);
+  @override
+  bool get opaque => false;
+  @override
+  bool get barrierDismissible => false;
+  @override
+  Color get barrierColor => Colors.black.withOpacity(0.5);
+  @override
+  String get barrierLabel => null;
+  @override
+  bool get maintainState => true;
+ 
+ 
+  @override
+  Widget buildPage(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      ) {
+    return Material(
+      type: MaterialType.transparency,
+      child: SafeArea(
+        child: _buildOverlayContent(context),
+      ),
+    );
+  }
+ 
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    return FadeTransition(
+      opacity: animation,
+      child: ScaleTransition(
+        scale: animation,
+        child: child,
+      ),
+    );
+  }
+ 
+  Widget _buildOverlayContent(BuildContext context) {
+    return Center(
+      child: dialogContent(context),
+    );
+  }
+ 
+  Widget dialogContent(BuildContext context) {
+    return WillPopScope(
+      child: this.contents,
+      onWillPop: () {
+        return Future(() => isAndroidBackEnable);
+      },
+    ); 
+  }
 }
---
+++ lib/scene/test_dialog.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import './scene_manager.dart';
+ 
+class CustomDialog 
+{
+  BuildContext context;
+  CustomDialog(this.context) : super();
+ 
+  /*
+   * 表示
+   */
+  void showCustomDialog() {
+ 
+    Navigator.push(
+      context,
+      ModalOverlay(
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+ 
+              Stack(
+                children: <Widget>[
+ 
+                  // Image.asset(
+                  //   "images/fukidashi.png",
+                  //   fit: BoxFit.fitWidth,
+                  // ),
+ 
+                  Container(
+                      height: 200.0,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+ 
+                            /*
+                             * タイトル
+                             */
+                            Text(
+                              "カスタムダイアログ",
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                                locale: Locale("ja", "JP"),
+                              ),
+                            ),
+ 
+                            /*
+                             * メッセージ
+                             */
+                            Text(
+                              "こんな感じでダイアログが出せるよ",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                locale: Locale("ja", "JP"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                  )
+                ],
+              ),
+ 
+              /*
+               * OKボタン
+               */
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(),
+                  ),
+ 
+                  FlatButton(
+                    color: Colors.blue,
+                    child: Text(
+                      "OK",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        locale: Locale("ja", "JP"),
+                      ),
+                    ),
+                    onPressed: () {
+                      hideCustomDialog();
+                      Navigator.of(context).pushNamed(KEY_HOME);
+                    },
+                  ),
+ 
+                  SizedBox(
+                    width: 80.0,
+                  )
+                ],
+              ),
+            ],
+          )
+        ),
+        isAndroidBackEnable: false,
+      ),
+    );
+  }
+ 
+ 
+  /*
+   * 非表示
+   */
+  void hideCustomDialog() {
+ 
+    Navigator.of(context).pop();
+  }
+}
